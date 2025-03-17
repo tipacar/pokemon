4c857f6478dc6b11bfdabce8971b7c2cb8c91ca6
tipacar <egetipacar@gmail.com>
2025-03-17 16:41:47 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,6 +1,6 @@
 import aiohttp  # Eşzamansız HTTP istekleri için bir kütüphane
 import random
-
+import asyncio
 class Pokemon:
     pokemons = {}
     # Nesne başlatma (kurucu)
@@ -8,6 +8,8 @@ class Pokemon:
         self.pokemon_trainer = pokemon_trainer
         self.pokemon_number = random.randint(1, 1000)
         self.name = None
+        self.hp=random.randint(50,100)
+        self.power=random.randint(20,50)
         self.ability = None
         if pokemon_trainer not in Pokemon.pokemons:
             Pokemon.pokemons[pokemon_trainer] = self
@@ -39,7 +41,7 @@ class Pokemon:
         # Pokémon hakkında bilgi döndüren bir metot
         if not self.name:
             self.name = await self.get_name()  # Henüz yüklenmemişse bir adın geri alınması
-        return f"Pokémonunuzun ismi: {self.name}"  # Pokémon'un adını içeren dizeyi döndürür
+        return f"Pokémonunuzun ismi: {self.name} \nPokémonunuzun sağlığı {self.hp} \nPokémonunuzun gücü {self.power}"  # Pokémon'un adını içeren dizeyi döndürür
 
     async def show_img(self):
         # PokeAPI aracılığıyla bir pokémonun adını almak için eşzamansız bir yöntem
@@ -61,6 +63,45 @@ class Pokemon:
                     data = await response.json()  # JSON yanıtının alınması ve kodunun çözülmesi
                     return data['abilities'][0]['ability'][0]['name'] # Bir Pokémon'un adını döndürme
                 else:
-                    return "Pokemon"  # İstek başarısız olursa varsayılan adı döndürür               
+                    return "Pokemon"  # İstek başarısız olursa varsayılan adı döndürür
+
+    async def attack(self,target):
+        if isinstance(target, Wizard):  # Enemy'nin bir Wizard veri tipi olduğunu (Büyücü sınıfının bir örneği olduğunu) kontrol etme
+            chance = random.randint(1, 5) 
+        if chance == 1:
+            return "Sihirbaz Pokémon, savaşta bir kalkan kullanıldı!"
+        if target.hp>self.power:
+            target.hp-=self.power
+            return f"{self.pokemon_trainer},{target.pokemon_trainer}'a sardırdı!"
+        else:
+            target.hp=0
+            return f"{self.pokemon_trainer},{target.pokemon_trainer}'ı yendi!"
+        
+class Wizard(Pokemon):
+    async def attack(self, enemy):
+        return await super().attack(enemy)
+
+
+class Fighter(Pokemon):
+    async def attack(self, enemy):
+        super_power = random.randint(5, 15)  
+        self.power += super_power
+        sonuç = await super().attack(enemy)  
+        self.power -= super_power
+        return sonuç + f"\nDövüşçü Pokémon süper saldırı kullandı. Eklenen güç: {super_power}"
+            
+if __name__ == '__main__':
+    async def main():
+
+        wizard = Wizard("username1")
+        fighter = Fighter("username2")
+
+        print(await wizard.info())
+        print()
+        print(await fighter.info())
+        print()
+        print(await fighter.attack(wizard))
+        print(await wizard.info())
+    asyncio.run(main())           
 
 
